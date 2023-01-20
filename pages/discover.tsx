c8b59c9a1b86e721527611da83b99b7328fd2f2e
Juan Briceno <juanbri@gmx.com>
2023-01-20 19:07:55 +0100

    blogging

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,6 +1,7 @@
 import BlogCard from "@/components/BlogCard";
 import MetaTag from "@/components/MetaTag";
 import Schema from "@/components/Schema";
+import Spacer from "@/components/Spacer";
 import { createClient } from "contentful";
 import Link from "next/link";
 
@@ -30,8 +31,7 @@ const discover = ({ blogs }: { blogs: any }) => {
     "Join me on a journey into the exciting world of web development... where I'll reveal insider tips and tricks to take your skills to the next LEVEL and become ...";
   const title = "Blog | Briceno | Front End Developer";
   const date = new Date();
-  const image =
-    "https://svgshare.com/i/pdv.svg";
+  const image = "https://svgshare.com/i/pdv.svg";
   return (
     <>
       <Schema
@@ -53,18 +53,20 @@ const discover = ({ blogs }: { blogs: any }) => {
         about the digital world, I invite you to join me on this journey into
         the exciting and ever-evolving world of web development.
       </h3>
-      <div className="md:grid md:grid-cols-2 md:items-start gap-8 flex flex-col justify-center items-center">
+      <div className="md:grid md:grid-cols-2 md:gap-8">
         {blogs?.map((article: any, i: any) => {
           const { title, slug, description } = article?.fields;
           const img = article?.fields.img.fields.file.url;
           return (
-            <Link
-              key={article?.sys.id}
-              href={slug}
-              className="md:col-span-1 flex flex-col justify-center items-center max-w-[200px]  lg:w-auto hover:scale-110 transform-gpu ease-in-out duration-300"
-            >
-              <BlogCard img={img} title={title} description={description} />
-            </Link>
+            <div className="md:col-span-1 md:mb-0 mb-6 hover:scale-105 transform-gpu ease-in-out duration-300">
+              <Link
+                key={article?.sys.id}
+                href={slug}
+                className="justify-center items-center"
+              >
+                <BlogCard img={img} title={title} description={description} />
+              </Link>
+            </div>
           );
         })}
       </div>
