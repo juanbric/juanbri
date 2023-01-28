c8fefa290cb84871eef0f686f4bea54cf9a88bf0
Juan Briceno <juanbri@gmx.com>
2023-01-28 10:51:12 +0000

    first iteration of spanish

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -29,13 +29,14 @@ export async function getStaticProps() {
 }
 
 const Discover = ({ blogs }: { blogs: any }) => {
-  const description =
-    "Join me on a journey into the exciting world of web development... where I'll reveal insider tips and tricks to take your skills to the next LEVEL and become ...";
+  const toggleFromContext = useContext(ToggleContext);
+  const { isDarkMode, isSpanish } = toggleFromContext;
+  const description = isSpanish
+    ? "Estás preparado para venirte conmigo en un viaje al apasionante mundo del desarrollo web? donde revelaré trucos y consejos para llevar tus habilidades al siguiente NIVEL y convertirte en..."
+    : "Join me on a journey into the exciting world of web development... where I'll reveal insider tips and tricks to take your skills to the next LEVEL and become ...";
   const title = "Blog | Briceno | Front End Developer";
   const date = new Date();
   const image = "https://svgshare.com/i/pdv.svg";
-  const toggleFromContext = useContext(ToggleContext);
-  const { isDarkMode } = toggleFromContext;
   return (
     <>
       <Schema
@@ -53,16 +54,19 @@ const Discover = ({ blogs }: { blogs: any }) => {
       />
       <h2 className="header">Blog</h2>
       <h3 className={!isDarkMode ? "copy mt-8 mb-12" : "copy-light mt-8 mb-12"}>
-        Whether you&apos;re a developer, designer, entrepreneur or just curious
-        about the digital world, I invite you to join me on this journey into
-        the exciting and ever-evolving world of web development.
+        {isSpanish
+          ? "Bien seas un desarrollador web, diseñador, emprendedor o simplemente tengas curiosidad por el mundo digital, te invito a que te unas en este viaje hacia el emocionante y dinámico mundo del desarrollo web"
+          : "Whether you&apos;re a developer, designer, entrepreneur or just curious about the digital world, I invite you to join me on this journey into the exciting and ever-evolving world of web development."}
       </h3>
       <div className="md:grid md:grid-cols-2 md:gap-8">
         {blogs?.map((article: any, i: any) => {
           const { title, slug, description } = article?.fields;
           const img = article?.fields.img.fields.file.url;
           return (
-            <div key={i} className="md:col-span-1 md:mb-0 mb-6 hover:scale-105 transform-gpu ease-in-out duration-300">
+            <div
+              key={i}
+              className="md:col-span-1 md:mb-0 mb-6 hover:scale-105 transform-gpu ease-in-out duration-300"
+            >
               <Link
                 key={article?.sys.id}
                 href={slug}
